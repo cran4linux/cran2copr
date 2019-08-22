a564c24db996e5c4cc9c26a0e4e4064cd10545d5
Iñaki Úcar <iucar@fedoraproject.org>
2019-08-22 18:15:37 +0200

    script to sync with CRAN

---
+++ copr-sync.r
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+source("config.r")
+source("common.r")
+check_copr()
+
+cran <- available.packages()
+copr <- get_copr_list()
+
+pkgs.del <- copr[!copr %in% cran[,"Package"]]
+pkgs <- with_deps(cran[,"Package"], cran)
+pkgs.add <- pkgs[need_update(pkgs, cran)]
+
+if (length(pkgs.del))
+  system2("./copr-del.r", paste(pkgs.del, collapse=" "))
+if (length(pkgs.add))
+  system2("./copr-add.r", paste(pkgs.add, collapse=" "))
