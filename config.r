a0d7366ce67001e202d842b3d5490458b8e792d1
Iñaki Úcar <iucar@fedoraproject.org>
2020-01-18 23:56:35 +0100

    disable rawhide

--- config.r
+++ config.r
@@ -6,7 +6,7 @@ options(
   copr.prefix    = "R-CRAN-",
   copr.tpl       = "specfile.tpl",
   copr.bflags    = "--background",
-  copr.chroots   = NULL,
+  copr.chroots   = c("fedora-30-x86_64", "fedora-31-x86_64"),
   repos          = "https://cloud.r-project.org",
   error          = NULL,
   available_packages_filters = c("R_version", "OS_type", "license/FOSS")
