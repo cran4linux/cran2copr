8c77242542c3cefd9b14f6d2204fce37acd470ec
Iñaki Úcar <iucar@fedoraproject.org>
2020-04-20 10:32:49 +0200

    remove temporary fix from igraph

--- common.r
+++ common.r
@@ -358,7 +358,7 @@ pkg_exceptions <- function(tpl, pkg, path) {
   tpl[install] <- paste0(tpl[install], "\n", switch(
     pkg,
     aws=,biglars=,bsamGP=,deldir=,deSolve=,DRAFT=,DynamicGP=,EDR=,eha=,float=,
-    frailtypack=,frontier=,igraph=,lbfgsb3=,lbfgsb3c=,lsei=,mclust=,mda=,n1qn1=,
+    frailtypack=,frontier=,lbfgsb3=,lbfgsb3c=,lsei=,mclust=,mda=,n1qn1=,
     npsp=,robeth=,robustbase=,rootSolve=,sequoia=,subplex=,VGAM=paste(
       "test $(gcc -dumpversion) -ge 10 && mkdir -p ~/.R &&",
       "echo \"FFLAGS=$(R CMD config FFLAGS) -fallow-argument-mismatch\" > ~/.R/Makevars"),
