c84ee710bad2faf5fcbcca52629cbed76f359ad3
Iñaki Ucar <iucar@fedoraproject.org>
2021-06-18 10:51:12 +0200

    Drop rawhide, needs rebuild for R 4.1

--- config.r
+++ config.r
@@ -6,7 +6,7 @@ options(
   copr.prefix      = "R-CRAN-",
   copr.tpl         = "spec.in",
   copr.bflags      = "",
-  copr.chroots     = c("fedora-33-x86_64", "fedora-34-x86_64", "fedora-rawhide-x86_64"),
+  copr.chroots     = c("fedora-33-x86_64", "fedora-34-x86_64"),
   repos            = "https://cran.r-project.org",
   error            = NULL,
   stringsAsFactors = FALSE,
