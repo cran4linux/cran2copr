ef2ce827401d53d7803a34db6a928b7147ddd0a7
Iñaki Úcar <iucar@fedoraproject.org>
2021-09-19 12:05:05 +0200

    add workflow for creating and pushing images

---
+++ docker/build-push.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+set -ex
+
+$VERSION=$1
+$LABEL=$2
+
+echo "$TOKEN" | docker login $REGISTRY -u $USER --password-stdin
+docker build . --pull --build-arg VERSION=$VERSION -t $REGISTRY/$USER/$IMAGE:$LABEL
+docker push $REGISTRY/$USER/$IMAGE:$LABEL
