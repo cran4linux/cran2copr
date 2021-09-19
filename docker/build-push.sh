6a1ca79219d3cb3c1a187e4f92c14f6f5ce9facc
Iñaki Úcar <iucar@fedoraproject.org>
2021-09-19 12:15:37 +0200

    more fixes

--- docker/build-push.sh
+++ docker/build-push.sh
@@ -3,7 +3,8 @@ set -ex
 
 VERSION=$1
 LABEL=$2
+IMAGE=$(echo $IMAGE | tr '[A-Z]' '[a-z]')
 
 echo "$TOKEN" | docker login $REGISTRY -u $USER --password-stdin
-docker build . --pull --build-arg VERSION=$VERSION -t $REGISTRY/$USER/$IMAGE:$LABEL
-docker push $REGISTRY/$USER/$IMAGE:$LABEL
+docker build . --pull --build-arg VERSION=$VERSION -t $REGISTRY/$IMAGE:$LABEL
+docker push $REGISTRY/$IMAGE:$LABEL
