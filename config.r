53141d414244bd74bf34b58b11a85deaa12ed470
Iñaki Úcar <iucar@fedoraproject.org>
2023-05-14 21:46:24 +0200

    f36 is EOL

--- config.r
+++ config.r
@@ -6,7 +6,7 @@ options(
   copr.prefix      = "R-CRAN-",
   copr.tpl         = "spec.in",
   copr.bflags      = "",
-  copr.chroots     = paste("fedora", c(36:38, "rawhide"), "x86_64", sep="-"),
+  copr.chroots     = paste("fedora", c(37:38, "rawhide"), "x86_64", sep="-"),
   repos            = "https://cran.r-project.org",
   error            = NULL,
   stringsAsFactors = FALSE,
