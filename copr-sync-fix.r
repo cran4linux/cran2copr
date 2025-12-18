b3c2d9848e8f6c5c6b563a555d876aa231aeaeae
Iñaki Úcar <iucar@fedoraproject.org>
2025-12-18 11:35:02 +0100

    small fix

--- copr-sync-fix.r
+++ copr-sync-fix.r
@@ -13,7 +13,7 @@ pkgs.fail <- subset_failed(df.mon)$Package
 
 pkgs.rebl <- setdiff(c(pkgs.mism, pkgs.nobl), pkgs.fail)
 pkgs.rebl <- sub(getOption("copr.prefix"), "", pkgs.rebl)
-pkgs.rebl <- with_rebuild_deps(pkgs.rebl, cran)
+pkgs.rebl <- with_rebuild_deps(pkgs.rebl)
 
 if (length(pkgs.rebl))
   script_call("./copr-rebuild-repo.r", paste(pkgs.rebl, collapse=" "))
